48f1dfd12baf9243f1fb1ff26f3f842a997bda95
ShubhamSachdeva311205 <shubhamsachdeva245@gmail.com>
2025-02-14 16:25:02 +0530

    Create ImageCap.py

---
+++ ImageCap.py
@@ -0,0 +1,12 @@
+import requests
+from PIL import Image
+from transformers import AutoProcessor, BlipForConditionalGeneration
+processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+img_path = "download (2).jpeg"
+image = Image.open(img_path).convert('RGB')
+text = "the color of the subject is"
+inputs = processor(images=image, text=text, return_tensors="pt")
+outputs = model.generate(**inputs, max_length=50)
+caption = processor.decode(outputs[0], skip_special_tokens=True)
+print(caption)
